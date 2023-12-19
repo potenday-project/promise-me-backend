3d865c70489c9535bcf10c6c68a07874f4ae6e1c
UU_jeong <80961726+oU-Ua@users.noreply.github.com>
2023-12-19 21:17:58 +0900

    refactor : separate fetch APi method

--- src/main/java/mvc/promiseme/calendar/repository/CalendarRepository.java
+++ src/main/java/mvc/promiseme/calendar/repository/CalendarRepository.java
@@ -6,9 +6,13 @@ import mvc.promiseme.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 //    @Query("SELECT NEW mvc.promiseme.calendar.dto.CalendarResponseDTO(c.content, c.startDate, c.finishDate, u.nickname) from Calendar c join c.member m join m.users u where c.project = :project")
 //    List<CalendarResponseDTO> findByProject(Project project);
+List<Calendar> findByProjectAndStartDateLessThanEqualAndFinishDateGreaterThanEqual(Project project, LocalDate startDate, LocalDate finishDate);
 }
