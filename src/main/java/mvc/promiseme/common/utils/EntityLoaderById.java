7efd9f3739021238dab1c97925a011aafed2196d
cyzlcyzl <cyzlcyzl09@gmail.com>
2024-01-08 01:45:36 +0900

    Feat: id값을 통해 Project Entity를 가져오는 메소드 구현
    Related to: #62

---
+++ src/main/java/mvc/promiseme/common/utils/EntityLoaderById.java
@@ -0,0 +1,18 @@
+package mvc.promiseme.common.utils;
+
+import lombok.RequiredArgsConstructor;
+import mvc.promiseme.project.entity.Project;
+import mvc.promiseme.project.repository.ProjectRepository;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+public class EntityLoaderById {
+
+    private final ProjectRepository projectRepository;
+
+    public Project getProjectByIdOrThrow(Long projectId) {
+        return projectRepository.findById(projectId)
+                .orElseThrow(() -> new NoSuchElementException("[ERROR] 해당 프로젝트는 존재하지 않습니다"));
+    }
+}
